1c6f7ca4d30ed2ec6c54a3973cabd9992863f5a7
4ddy1 <adil_15-05@mail.ru>
2021-07-28 01:58:22 +0600

    add slick library for slider

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function openForm(){
     document.getElementById("myForm").style.display = "block"
+    document.getElementById("shadow").style.display = "block"
 }
 
 function closeForm(){
     document.getElementById("myForm").style.display = "none"
+    document.getElementById("shadow").style.display = "none"
 }
+
+
+
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true
+    });
+  });
